fa737796023c0543b21d6173c2881103f4813109
Sanomi <snrutledge@gmail.com>
2015-09-08 23:26:59 -0700

    sexy code!

--- main.js
+++ main.js
@@ -5,30 +5,23 @@ let fun, answer;
 const server = http.createServer(function(req, res) {
 	res.statusCode = 200;
 	fun = req.url.split('/');
-		switch(true) {
-			case /\/math\/add\/\d+/.test(req.url) : 
-				answer = (parseFloat(fun[3]) + parseFloat(fun[4])).toString();
-				res.write(answer);
-				res.end();
-				break;
-			case /\/math\/subtract\/\d+/.test(req.url) : 
-				answer = (parseFloat(fun[3]) - parseFloat(fun[4])).toString();
-				res.write(answer);
-				res.end();
-				break;
-			case /\/math\/multiply\/\d+/.test(req.url) : 
-				answer = (parseFloat(fun[3]) * parseFloat(fun[4])).toString();
-				res.write(answer);
-				res.end();
-				break;
-			case /\/math\/divide\/\d+/.test(req.url) : 
-				answer = (parseFloat(fun[3]) / parseFloat(fun[4])).toString();
-				res.write(answer);
-				res.end();
-				break;
+	switch(true) {
+		case /\/math\/add\/\d+/.test(req.url) : 
+		answer = (parseFloat(fun[3]) + parseFloat(fun[4])).toString();
+		break;
+		case /\/math\/subtract\/\d+/.test(req.url) : 
+		answer = (parseFloat(fun[3]) - parseFloat(fun[4])).toString();
+		break;
+		case /\/math\/multiply\/\d+/.test(req.url) : 
+		answer = (parseFloat(fun[3]) * parseFloat(fun[4])).toString();
+		break;
+		case /\/math\/divide\/\d+/.test(req.url) : 
+		answer = (parseFloat(fun[3]) / parseFloat(fun[4])).toString();
+		break;
 		default :
-			res.end("What? Don't you want to do some math?");
-		}
+		res.end("What? Don't you want to do some math?");
+	}
+		res.end(answer);
 });
 
 server.listen(PORT, function(){
