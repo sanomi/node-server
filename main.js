20831c3b32f8f4585fb82fa3efa2cde87b43ea07
Sanomi <snrutledge@gmail.com>
2015-09-08 23:01:24 -0700

    refactored with es6

--- main.js
+++ main.js
@@ -1,40 +1,34 @@
-var http = require('http');
-var PORT = 8000;
-
-var server = http.createServer(function(req, res) {
+'use strict';
+const http = require('http');
+const PORT = 8000;
+let fun, answer;
+const server = http.createServer(function(req, res) {
+	res.statusCode = 200;
+	fun = req.url.split('/');
 		switch(true) {
-			case /\/math\/add/.test(req.url) :
-			res.statusCode = 200;
-			var add = req.url.split('/');
-			var answer = (parseFloat(add[3]) + parseFloat(add[4])).toString();
-			res.write(answer);
-			res.end();
-			break;
-			case /\/math\/subtract/.test(req.url) :
-			res.statusCode = 200;
-			var subtract = req.url.split('/');
-			var answer = (parseFloat(add[3]) - parseFloat(add[4])).toString();
-			res.write(answer);
-			res.end();
-			break;
-			case /\/math\/multiply/.test(req.url) :
-			res.statusCode = 200;
-			var multiply = req.url.split('/');
-			var answer = (parseFloat(add[3]) * parseFloat(add[4])).toString();
-			res.write(answer);
-			res.end();
-			break;
-			case /\/math\/divide/.test(req.url) :
-			res.statusCode = 200;
-			var divide = req.url.split('/');
-			var answer = (parseFloat(add[3]) / parseFloat(add[4])).toString();
-			res.write(answer);
-			res.end();
-			break;
+			case /\/math\/add\/\d+/.test(req.url) : 
+				answer = (parseFloat(fun[3]) + parseFloat(fun[4])).toString();
+				res.write(answer);
+				res.end();
+				break;
+			case /\/math\/subtract\/\d+/.test(req.url) : 
+				answer = (parseFloat(fun[3]) - parseFloat(fun[4])).toString();
+				res.write(answer);
+				res.end();
+				break;
+			case /\/math\/multiply\/\d+/.test(req.url) : 
+				answer = (parseFloat(fun[3]) * parseFloat(fun[4])).toString();
+				res.write(answer);
+				res.end();
+				break;
+			case /\/math\/divide\/\d+/.test(req.url) : 
+				answer = (parseFloat(fun[3]) / parseFloat(fun[4])).toString();
+				res.write(answer);
+				res.end();
+				break;
 		default :
-			res.end("Pick a function, any function.")
+			res.end("What? Don't you want to do some math?");
 		}
-		res.end("What? Don't you want to do some math?");
 });
 
 server.listen(PORT, function(){
