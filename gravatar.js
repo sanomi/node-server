961f7d603fa848b82980e59819b2f3ee7e494025
Sanomi <snrutledge@gmail.com>
2015-09-09 21:54:27 -0700

    everything works!

--- gravatar.js
+++ gravatar.js
@@ -1,10 +1,12 @@
+'use strict';
 const mdn5 = require('md5');
 
-let gravatarPic = () => {
+let gravatarPic = (req, res) => {
+		let fun = req.url.split('/');
 		let gravatar = "http://gravatar.com/avatar/" + (mdn5(fun[2])).toString();
 		let html = "<img src='"+gravatar+"'></img>"
 		res.setHeader("Content-Type", "text/html");
-		res.end(html);
+		return html;
 }
 
 module.exports = gravatarPic;
--- main.js
+++ main.js
@@ -3,40 +3,24 @@ const http = require('http');
 const wordData = require('./wordCounter.js');
 const mdn5 = require('md5');
 const math = require('./math.js');
+const gravatar = require('./gravatar.js');
 const PORT = 8000;
 let express = require('express');
 let app = express();
-
 let fun, answer;
-
 app.get('/math/*', (req,res,next) => {
 	math(req, res, next);
 });
 app.get('/words/*', (req,res,next) => {
 	res.end(wordData(req, res, next, fun));
 });
-// app.get('/gravatarUrl/*', (req,res,next) => {
-
-// });
-
-
-const server = http.createServer(function(req, res) {
-	res.statusCode = 200;
-	fun = req.url.split('/');
-	// if ( /\/math/.test(req.url)) {
-	// 		math(req, res);
-	// }
-	// if (/\/words/.test(req.url)) {
-	// 	res.end(wordData(fun));
-	// }
-	if (/\/gravatarUrl/.test(req.url)){
-		let gravatar = "http://gravatar.com/avatar/" + (mdn5(fun[2])).toString();
-		let html = "<img src='"+gravatar+"'></img>"
-		res.setHeader("Content-Type", "text/html");
-		res.end(html);
-	}
+app.get('/gravatarUrl/*', (req,res,next) => {
+	res.end(gravatar(req, res));
 });
-
+app.get('/', (req,res,next) => {
+		res.statusCode = 200;
+	fun = req.url.split('/');
+})
 app.listen(PORT, function(){
 	console.log('server now listening on port ' + PORT + ' ! ;)')
 });
